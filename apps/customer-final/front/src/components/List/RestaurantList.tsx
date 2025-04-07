2fa485f54b7a4a7a471b5b7d4a7e6c173826180c
CharifMah <charif25430@hotmail.fr>
2025-04-07 21:45:06 +0200

    Fix RestaurantList.tsx

--- apps/customer-final/front/src/components/List/RestaurantList.tsx
+++ apps/customer-final/front/src/components/List/RestaurantList.tsx
@@ -11,7 +11,7 @@ const RestaurantList: React.FC<RestaurantListProps> = ({ restaurants }) => {
         <div className="w-full py-4 center">
             <div className="grid grid-cols-[repeat(auto-fit,minmax(12rem,1fr))] gap-6 max-w-screen-xl mx-auto">
                 {restaurants.map((restaurant) => (
-                    <RestaurantCard name={restaurant.name} imageSrc={restaurant.logo} title={restaurant.name} subtitle={restaurant.description} />
+                    <RestaurantCard {...restaurant} />
                 ))}
             </div>
         </div>
