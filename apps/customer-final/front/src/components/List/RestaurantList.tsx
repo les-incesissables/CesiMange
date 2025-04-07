6375af8d2693174fc68f731219b8697d94714172
CharifMah <charif25430@hotmail.fr>
2025-04-07 21:45:06 +0200

    Fix RestaurantList.tsx
    Update RestaurantList.tsx

--- apps/customer-final/front/src/components/List/RestaurantList.tsx
+++ apps/customer-final/front/src/components/List/RestaurantList.tsx
@@ -2,16 +2,12 @@ import React from 'react';
 import RestaurantCard from '../Card/RestaurantCard';
 import { IRestaurant } from '../../models/interfaces/IRestaurant/IRestaurant';
 
-interface RestaurantListProps {
-    restaurants: IRestaurant[];
-}
-
-const RestaurantList: React.FC<RestaurantListProps> = ({ restaurants }) => {
+const RestaurantList: React.FC<IRestaurant[]> = (pRestaurants: IRestaurant[]) => {
     return (
         <div className="w-full py-4 center">
             <div className="grid grid-cols-[repeat(auto-fit,minmax(12rem,1fr))] gap-6 max-w-screen-xl mx-auto">
-                {restaurants.map((restaurant) => (
-                    <RestaurantCard name={restaurant.name} imageSrc={restaurant.logo} title={restaurant.name} subtitle={restaurant.description} />
+                {pRestaurants.map((restaurant) => (
+                    <RestaurantCard {...restaurant} />
                 ))}
             </div>
         </div>
