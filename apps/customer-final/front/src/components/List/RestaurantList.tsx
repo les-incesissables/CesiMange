bb45431bfc7247528f43ccb58ffc16e992d7f769
CharifMah <charif25430@hotmail.fr>
2025-04-07 21:53:20 +0200

    fix crash mongo + fix restaurant list

--- apps/customer-final/front/src/components/List/RestaurantList.tsx
+++ apps/customer-final/front/src/components/List/RestaurantList.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import RestaurantCard from '../Card/RestaurantCard';
 import { IRestaurant } from '../../models/interfaces/IRestaurant/IRestaurant';
 
-const RestaurantList: React.FC<IRestaurant[]> = (pRestaurants: IRestaurant[]) => {
+interface RestaurantListProps
+{
+    restaurants: IRestaurant[];
+}
+
+const RestaurantList: React.FC<RestaurantListProps> = ({ restaurants }) =>
+{
     return (
         <div className="w-full py-4 center">
             <div className="grid grid-cols-[repeat(auto-fit,minmax(12rem,1fr))] gap-6 max-w-screen-xl mx-auto">
-                {pRestaurants.map((restaurant) => (
+                {restaurants.map((restaurant) => (
                     <RestaurantCard {...restaurant} />
                 ))}
             </div>
