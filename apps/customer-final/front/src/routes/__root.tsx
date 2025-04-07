3ced70357267051ed8f05a7fc1086df66a7a0c6e
FaMafaik <faik.mehmeti67@gmail.com>
2025-04-07 09:42:03 +0200

    redefine front structure

--- apps/customer-final/front/src/routes/__root.tsx
+++ apps/customer-final/front/src/routes/__root.tsx
@@ -1,15 +1,37 @@
-import * as React from 'react'
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import * as React from 'react';
+import { Outlet, createRootRoute } from '@tanstack/react-router';
+import '@les-incesissables/cesimange-ui-library/style.css';
+
+import { Button, ButtonProps } from '@les-incesissables/cesimange-ui-library';
 
 export const Route = createRootRoute({
-  component: RootComponent,
-})
+    component: RootComponent,
+});
 
 function RootComponent() {
-  return (
-    <React.Fragment>
-      <div>Hello "__root"!</div>
-      <Outlet />
-    </React.Fragment>
-  )
+    // Déclaration d'un objet conforme à ButtonProps
+    // On peut définir un objet ButtonProps explicitement
+    const buttonProps: ButtonProps = {
+        label: 'Cliquez-moi',
+        variant: 'solid', // doit être l'une des valeurs: "solid", "outline", "ghost"
+        size: 'md', // "xs", "sm", "md", "lg", "xl"
+        colorscheme: 'blue', // "blackYellow", "red", "blue", "green"
+        selected: false,
+        rounded: 'full', // "xs", "sm", "md", "lg", "xl", "full"
+        defaultBg: 'bg-blue-500',
+        hoverBg: 'hover:bg-blue-600',
+    };
+
+    return (
+        <React.Fragment>
+            <div>Hello "__root"!</div>
+            <Button
+                onClick={() => {
+                    console.log('test');
+                }}
+                label="teqs sdfsdf sdffqsf st"
+            />
+            <Outlet />
+        </React.Fragment>
+    );
 }
