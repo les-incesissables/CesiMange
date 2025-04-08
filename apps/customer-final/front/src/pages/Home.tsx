294de1406d8fd01092293aac2a0624d64a0b2b47
CharifMah <charif25430@hotmail.fr>
2025-04-08 14:12:24 +0200

    Update Home.tsx

--- apps/customer-final/front/src/pages/Home.tsx
+++ apps/customer-final/front/src/pages/Home.tsx
@@ -23,7 +23,7 @@ const Home: React.FC = () =>
         queryFn: ({ pageParam = 1 }) => localMiddleware.callLocalApi(() =>
             localMiddleware.RestoRepo.fetchAll(pageParam, LIMIT)
         ),
-        getNextPageParam: (lastPage) => lastPage.data[1].hasNext ? lastPage.data[1].currentPage + 1 : null,
+        getNextPageParam: (lastPage) => lastPage.data[1].hasNext ? lastPage.data[1].page + 1 : null,
         initialPageParam: 1,
     });
 
@@ -31,7 +31,7 @@ const Home: React.FC = () =>
     if (isError) return <div>Erreur</div>;
 
     const allRestaurants = data?.pages.flatMap(page => page.data[0] as IRestaurant[]) ?? [];
-    const currentPage = data?.pages[data.pages.length - 1].data[1].currentPage || 1;
+    const currentPage = data?.pages[data.pages.length - 1].data[1].page || 1;
 
     return (
         <HomeLayout>
@@ -39,6 +39,10 @@ const Home: React.FC = () =>
             <RestaurantList restaurants={allRestaurants} />
 
             <div className="flex items-center justify-center gap-4 my-8">
+                <span className="px-6 py-2 bg-stone-300 border border-black rounded-[20px] text-black font-['Inter'] font-bold shadow-[0px_4px_4px_rgba(0,0,0,0.25)] outline outline-1 outline-black outline-offset-[-1px]">
+                    Page {currentPage}
+                </span>
+
                 <button
                     onClick={() => fetchNextPage()}
                     disabled={!hasNextPage || isFetchingNextPage}
