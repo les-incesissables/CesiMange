76a2f5fd4dde71f3fd5756c8664d6b85da3dfd01
CharifMah <charif25430@hotmail.fr>
2025-04-09 21:34:47 +0200

    update entry point

--- platforms/databases/mongodb/restaurant-service/entrypoint.sh
+++ platforms/databases/mongodb/restaurant-service/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+mongod 
+
 mongoimport --host localhost --db restaurant-service --collection restaurants --file /docker-entrypoint-initdb.d/restaurant-service.restaurants.json
 mongoimport --host localhost --db restaurant-service --collection promotions --file /docker-entrypoint-initdb.d/restaurant-service.promotions.json
 mongoimport --host localhost --db restaurant-service --collection reviews --file /docker-entrypoint-initdb.d/restaurant-service.reviews.json
